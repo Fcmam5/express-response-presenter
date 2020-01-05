9fc2d74ddcb3922a8195764e2e5158fed985460f
Fcmam5 <dr.fcmam5@gmail.com>
2020-01-05 03:23:51 +0100

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Express response presenter
+
+Standardize ExpressJS JSON responses
+
+## Installation
+
+Run `npm i -S express-presenter`
+
+## Usage
+
+* Import the package 
+  * `const expressPresenter = require('express-presenter')`
+* Instantiate `express-presenter` (it takes an optional `apiVersion` parameter)
+  * `const presenter = expressPresenter('v1')`
+* Use it as a middleware (see the examples below)
+
+### Example
+
+As any [ExpressJS middleware](https://expressjs.com/en/guide/using-middleware.html), you can either use it globally:
+
+```javascript
+// See: examples/simple-express-app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressPresenter = require('express-presenter');
+
+const app = express();
+const presenter = expressPresenter();
+
+app.use(bodyParser.json());
+app.use(presenter);  // This must be before declaring your routes
+
+app.use('/ok', (req, res) => {
+  res.ok({
+    message: 'Take my mhajeb',
+    data: { name: 'Mhajeb', isHot: true }
+  });
+});
+
+app.use('/created', (req, res) => {
+  res.created({
+    message: 'A new mhajeb is created',
+    data: { name: 'Mhajeb', isHot: true }
+  });
+});
+
+```
+
+Or in your routes file:
+
+```javascript
+// See: examples/express-routes-file.js
+
+const express = require('express');
+const expressPresenter = require('../src');
+
+const router = express.Router();
+const presenter = expressPresenter('v1'); // Here you can set your API version
+
+router.get('/api/v1/get', presenter, (req, res) => {
+  res.ok({
+    message: 'Take my mhajeb',
+    data: { name: 'Mhajeb', isHot: true }
+  });
+});
+
+router.post('/api/v1/create', presenter, (req, res) => {
+  // ... Creating mhajeb
+  res.created({
+    message: 'A new mhajeb is created',
+    data: { name: 'Mhajeb', isHot: true }
+  });
+});
+
+router.get('/api/v1/start-task', presenter, (req, res) => {
+  res.accepted({
+    message: 'I will start cooking Mhajeb',
+  });
+});
+
+router.get('/api/v1/finished-eating', presenter, (req, res) => {
+  res.noContent();
+});
+
+module.exports = router;
+```
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details
---
+++ test/index.test.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const presenter = require('../src');
+const request = require('supertest');
+
+describe('presenter', () => {
+  let server;
+
+  beforeEach(() => {
+    server = bootstrapExpressApp({
+      middleware: presenter('v1'),
+      port: 3000
+    });
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  describe('res.ok', () => {
+
+    it('should return a response of status code of 200', async () => {
+      const res = await request(server).get('/ok');
+      expect(res.status).toBe(200);
+    });
+
+    it('should return a response having {apiVersion, message, data}', async () => {
+      const res = await request(server).get('/ok');
+      expect(res.body).toStrictEqual({
+        "apiVersion": "v1",
+        "data": {
+          "isHot": true,
+          "name": "Mhajeb"
+        },
+        "message": "Take my mhajeb"
+      });
+    });
+
+    it('should return a response having {message, data} when apiVersion is not set', async (done) => {
+      const otherServer = bootstrapExpressApp({
+        middleware: presenter(),
+        port: 3031
+      });
+
+      const res = await request(otherServer).get('/ok');
+
+      expect(res.body).toStrictEqual({
+        "data": {
+          "isHot": true,
+          "name": "Mhajeb"
+        },
+        "message": "Take my mhajeb"
+      });
+
+      // cleanup
+      otherServer.close();
+      done();
+    })
+  });
+
+  describe('res.created', () => {
+    it('should return a response of status code of 201', async () => {
+      const res = await request(server).get('/created');
+      expect(res.status).toBe(201);
+    });
+
+    it('should return a response having {apiVersion, message, data}', async () => {
+      const res = await request(server).get('/created');
+      expect(res.body).toStrictEqual({
+        "apiVersion": "v1",
+        "data": {
+          "isHot": true,
+          "name": "Mhajeb"
+        },
+        "message": "A new mhajeb is created"
+      });
+    });
+  });
+
+  describe('res.accepted', () => {
+    it('should return a response of status code of 202', async () => {
+      const res = await request(server).get('/accepted');
+      expect(res.status).toBe(202);
+    });
+
+    it('should return a response having {apiVersion, message, data}', async () => {
+      const res = await request(server).get('/accepted');
+      expect(res.body).toStrictEqual({
+        "apiVersion": "v1",
+        "message": 'I will start cooking Mhajeb',
+      });
+    });
+  });
+
+  describe('res.noContent', () => {
+    it('should return a response of status code of 204', async () => {
+      const res = await request(server).get('/no-content');
+      expect(res.status).toBe(204);
+    });
+
+    it('should return a response having {apiVersion, message, data}', async () => {
+      const res = await request(server).get('/no-content');
+      expect(res.body).toEqual({});
+    });
+  });
+});
+
+const bootstrapExpressApp = (options) => {
+  const app = express();
+
+  app.use(bodyParser.json());
+
+
+  if (options.middleware) {
+    app.use(options.middleware);
+  }
+
+  app.use('/ok', (req, res) => {
+    res.ok({
+      message: 'Take my mhajeb',
+      data: { name: 'Mhajeb', isHot: true }
+    });
+  });
+
+  app.use('/created', (req, res) => {
+    res.created({
+      message: 'A new mhajeb is created',
+      data: { name: 'Mhajeb', isHot: true }
+    });
+  });
+
+  app.use('/accepted', (req, res) => {
+    res.accepted({
+      message: 'I will start cooking Mhajeb',
+    });
+  });
+
+  app.use('/no-content', (req, res) => {
+    res.noContent();
+  });
+
+  const server = app.listen(options.port || 3000);
+
+  return server;
+}
