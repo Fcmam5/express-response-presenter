5c345b8903ab0461d7b11194cac51a64de702325
Abdeldjalil Fortas <dr.fcmam5@gmail.com>
2023-08-16 00:24:31 +0200

    :package: upgrade packages to fix vulnerablities

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const presenter = require('../src');
-const request = require('supertest');
+const express = require("express");
+const bodyParser = require("body-parser");
+const presenter = require("../src");
+const request = require("supertest");
 
-describe('presenter', () => {
+describe("presenter", () => {
   let server;
 
   beforeEach(() => {
     server = bootstrapExpressApp({
-      middleware: presenter('v1'),
+      middleware: presenter("v1"),
       port: 3000,
     });
   });
@@ -17,88 +17,87 @@ describe('presenter', () => {
     server.close();
   });
 
-  describe('res.ok', () => {
-    it('should return a response of status code of 200', async () => {
-      const res = await request(server).get('/ok');
+  describe("res.ok", () => {
+    it("should return a response of status code of 200", async () => {
+      const res = await request(server).get("/ok");
       expect(res.status).toBe(200);
     });
 
-    it('should return a response having {apiVersion, message, data}', async () => {
-      const res = await request(server).get('/ok');
+    it("should return a response having {apiVersion, message, data}", async () => {
+      const res = await request(server).get("/ok");
       expect(res.body).toStrictEqual({
-        apiVersion: 'v1',
+        apiVersion: "v1",
         data: {
           isHot: true,
-          name: 'Mhajeb',
+          name: "Mhajeb",
         },
-        message: 'Take my mhajeb',
+        message: "Take my mhajeb",
       });
     });
 
-    it('should return a response having {message, data} when apiVersion is not set', async (done) => {
+    it("should return a response having {message, data} when apiVersion is not set", async () => {
       const otherServer = bootstrapExpressApp({
         middleware: presenter(),
         port: 3031,
       });
 
-      const res = await request(otherServer).get('/ok');
+      const res = await request(otherServer).get("/ok");
 
       expect(res.body).toStrictEqual({
         data: {
           isHot: true,
-          name: 'Mhajeb',
+          name: "Mhajeb",
         },
-        message: 'Take my mhajeb',
+        message: "Take my mhajeb",
       });
 
       // cleanup
       otherServer.close();
-      done();
     });
   });
 
-  describe('res.created', () => {
-    it('should return a response of status code of 201', async () => {
-      const res = await request(server).get('/created');
+  describe("res.created", () => {
+    it("should return a response of status code of 201", async () => {
+      const res = await request(server).get("/created");
       expect(res.status).toBe(201);
     });
 
-    it('should return a response having {apiVersion, message, data}', async () => {
-      const res = await request(server).get('/created');
+    it("should return a response having {apiVersion, message, data}", async () => {
+      const res = await request(server).get("/created");
       expect(res.body).toStrictEqual({
-        apiVersion: 'v1',
+        apiVersion: "v1",
         data: {
           isHot: true,
-          name: 'Mhajeb',
+          name: "Mhajeb",
         },
-        message: 'A new mhajeb is created',
+        message: "A new mhajeb is created",
       });
     });
   });
 
-  describe('res.accepted', () => {
-    it('should return a response of status code of 202', async () => {
-      const res = await request(server).get('/accepted');
+  describe("res.accepted", () => {
+    it("should return a response of status code of 202", async () => {
+      const res = await request(server).get("/accepted");
       expect(res.status).toBe(202);
     });
 
-    it('should return a response having {apiVersion, message, data}', async () => {
-      const res = await request(server).get('/accepted');
+    it("should return a response having {apiVersion, message, data}", async () => {
+      const res = await request(server).get("/accepted");
       expect(res.body).toStrictEqual({
-        apiVersion: 'v1',
-        message: 'I will start cooking Mhajeb',
+        apiVersion: "v1",
+        message: "I will start cooking Mhajeb",
       });
     });
   });
 
-  describe('res.noContent', () => {
-    it('should return a response of status code of 204', async () => {
-      const res = await request(server).get('/no-content');
+  describe("res.noContent", () => {
+    it("should return a response of status code of 204", async () => {
+      const res = await request(server).get("/no-content");
       expect(res.status).toBe(204);
     });
 
-    it('should return a response having {apiVersion, message, data}', async () => {
-      const res = await request(server).get('/no-content');
+    it("should return a response having {apiVersion, message, data}", async () => {
+      const res = await request(server).get("/no-content");
       expect(res.body).toEqual({});
     });
   });
@@ -113,27 +112,27 @@ const bootstrapExpressApp = (options) => {
     app.use(options.middleware);
   }
 
-  app.use('/ok', (req, res) => {
+  app.use("/ok", (req, res) => {
     res.ok({
-      message: 'Take my mhajeb',
-      data: { name: 'Mhajeb', isHot: true },
+      message: "Take my mhajeb",
+      data: { name: "Mhajeb", isHot: true },
     });
   });
 
-  app.use('/created', (req, res) => {
+  app.use("/created", (req, res) => {
     res.created({
-      message: 'A new mhajeb is created',
-      data: { name: 'Mhajeb', isHot: true },
+      message: "A new mhajeb is created",
+      data: { name: "Mhajeb", isHot: true },
     });
   });
 
-  app.use('/accepted', (req, res) => {
+  app.use("/accepted", (req, res) => {
     res.accepted({
-      message: 'I will start cooking Mhajeb',
+      message: "I will start cooking Mhajeb",
     });
   });
 
-  app.use('/no-content', (req, res) => {
+  app.use("/no-content", (req, res) => {
     res.noContent();
   });
 
